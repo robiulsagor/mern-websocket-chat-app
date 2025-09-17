f849bc1162114c8cf59a44ae76672aa25388b64c
robiulsagor <robiul100.me@gmail.com>
2025-09-17 19:36:22 +0600

    feat: add Login component and update Auth page layout

---
+++ client/src/components/Login.tsx
@@ -0,0 +1,25 @@
+const Login = () => {
+    return (
+        <div className="flex bg-white p-10 rounded-lg  gap-5 min-h-[400px] min-w-[600px]">
+            <div className="flex-1 flex flex-col justify-center items-center border-r border-r-slate-300 pr-10">
+                <div className="text-center space-y-5">
+                    <h2 className="text-3xl ">Beauty Chat</h2>
+                    Welcome again!
+                </div>
+            </div>
+
+            <form action="" className="flex-1  space-y-5">
+                <input type="text" placeholder="Email" className="w-full bg-slate-100 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                <input type="password" placeholder="Password" className="w-full bg-slate-100 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" />
+
+                <p className="text-right text-sm hover:underline cursor-pointer">Forgot password?</p>
+
+                <button className="w-full bg-slate-900 text-white p-2 rounded hover:bg-slate-700 transition  cursor-pointer">Sign In</button>
+
+                <p>Don't have an account? <span className="text-blue-500 hover:underline cursor-pointer">Sign Up</span></p>
+            </form>
+        </div>
+    )
+}
+
+export default Login
--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,6 +1,12 @@
+import Login from "../components/Login"
+
 const Auth = () => {
     return (
-        <div>Auth</div>
+        <main className="bg-slate-200 h-screen flex justify-center items-center">
+            <section>
+                <Login />
+            </section>
+        </main>
     )
 }
 
