fd2fd05c45e8bbf1ae7e1d412d07907077bc777b
robiulsagor <robiul100.me@gmail.com>
2025-09-22 16:11:49 +0600

    feat: add Login and Register components with animations; update Auth page to toggle between them

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"
-import Auth from "./pages/Auth";
 import ProtectedRoute from "./components/ProtectedRoute";
 import PublicRoute from "./components/PublicRoute";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-        <Route path="/login" element={<PublicRoute><Auth /></PublicRoute>} />
+        <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
+        <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
       </Routes>
     </Router>
   )
--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -3,11 +3,12 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
 type FormValues = {
+    name: string
     email: string;
     password: string;
 }
 
-const Login = () => {
+const Register = () => {
     const navigate = useNavigate()
 
     const {
@@ -22,9 +23,9 @@ const Login = () => {
 
     return (
         <main className="bg-slate-200 h-screen flex justify-center items-center">
-            <section className={`flex  items-center bg-white p-10 rounded-lg  gap-5 min-h-[400px] min-w-[800px]`}>
+            <section className={`flex items-center bg-white p-10 rounded-lg  gap-5 min-h-[400px] min-w-[800px]`}>
                 <AnimatePresence>
-                    <motion.div key="text" initial={{ opacity: 0, x: -100 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: .5 }}
+                    <motion.div key="text" initial={{ opacity: 0, x: 100 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: .5 }}
                         className="flex-1 flex flex-col justify-center items-center pr-10">
                         <div className="text-center space-y-5">
                             <h2 className="text-3xl ">Beauty Chat</h2>
@@ -32,14 +33,20 @@ const Login = () => {
                         </div>
                     </motion.div>
                 </AnimatePresence>
-                <motion.div key={"form"} initial={{ opacity: 0, x: 100 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: .5 }} className=" flex-1">
+                <motion.div key={"form"} initial={{ opacity: 0, x: -100 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: .5 }}
+                    className=" flex-1">
 
-                    {/* login form begins */}
+                    {/* Register form begins */}
                     <div className="flex-1 h-full">
 
-                        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5  ">
+                        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5 ">
                             <div>
-
+                                <input
+                                    {...register("name", { required: "Name is required" })}
+                                    type="text" placeholder="Name" className="w-full bg-slate-100 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                                {errors.name && <p className="mt-1 ml-1 text-red-700 text-sm">{errors.name.message}</p>}
+                            </div>
+                            <div>
                                 <input
                                     {...register("email", { required: "Email is required" })}
                                     type="email" placeholder="Email" className="w-full bg-slate-100 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" />
@@ -52,28 +59,21 @@ const Login = () => {
                                 {errors.password && <p className="mt-1 ml-1 text-red-700 text-sm">{errors.password.message}</p>}
                             </div>
 
-                            <p className="text-right text-sm hover:underline cursor-pointer">Forgot password?</p>
 
-                            <button className="w-full bg-slate-900 text-white p-2 rounded hover:bg-slate-700 transition  cursor-pointer">Sign In</button>
+                            <button className="w-full bg-slate-900 text-white p-2 rounded hover:bg-slate-700 transition  cursor-pointer">Sign Up</button>
 
 
                         </form>
                     </div>
-                    {/* login form ends */}
+                    {/* Register form begins */}
 
-
-                    <p className="text-sm mt-2 lg:mt-4">Don't have an account?
-                        <span className="text-blue-500 hover:underline cursor-pointer" onClick={() => navigate("/register")}> Sign Up</span>
+                    <p className="text-sm mt-2 lg:mt-4">Already have an account?
+                        <span className="text-blue-500 hover:underline cursor-pointer" onClick={() => navigate("/login")}> Sign In</span>
                     </p>
-
-
-
                 </motion.div>
-
-
             </section>
         </main>
     )
 }
 
-export default Login
+export default Register
