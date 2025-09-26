76dcba9efc57a83e749d340b44c5b78046aeb29b
robiulsagor <robiul100.me@gmail.com>
2025-09-26 20:55:48 +0600

    feat: Change the title of these pages with React Helmet

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,8 @@
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import AuthLayout from "../components/AuthLayout";
 import InputField from "../components/InputField";
+import { Helmet } from "react-helmet-async";
 
 type FormValues = {
     email: string;
@@ -27,42 +28,40 @@ const Login = () => {
     }
 
     return (
-        <AuthLayout title="Beauty Chat" sideContent="Welcome back!" >
+        <>
+            <Helmet>
+                <title>Login | Beauty Chat</title>
+                <meta name="description" content="Login to your account" />
+            </Helmet>
+            <AuthLayout title="Beauty Chat" sideContent="Welcome back!" >
 
-            {/* login form begins */}
-            <form onSubmit={handleSubmit(onSubmit)} className="space-y-5  ">
-                {
-                    fields.map((field) => (
-                        <div key={field.name}>
-                            <InputField
-                                type={field.type}
-                                placeholder={field.placeholder}
-                                register={register}
-                                name={field.name}
-                                error={errors[field.name as keyof FormValues]?.message as string}
-                            />
-                        </div>
-                    ))
-                }
+                {/* login form begins */}
+                <form onSubmit={handleSubmit(onSubmit)} className="space-y-5  ">
+                    {
+                        fields.map((field) => (
+                            <div key={field.name}>
+                                <InputField
+                                    type={field.type}
+                                    placeholder={field.placeholder}
+                                    register={register}
+                                    name={field.name}
+                                    error={errors[field.name as keyof FormValues]?.message as string}
+                                />
+                            </div>
+                        ))
+                    }
+                    <div className="flex justify-end -mt-3">
+                        <Link to="/forgot-password" className="text-right text-sm hover:underline cursor-pointer">Forgot password?</Link>
+                    </div>
+                    <button className="w-full bg-slate-900 text-white p-2 rounded hover:bg-slate-700 transition  cursor-pointer">Sign In</button>
+                </form>
+                {/* login form ends */}
 
-                <p className="text-right text-sm hover:underline cursor-pointer">Forgot password?</p>
-
-                <button className="w-full bg-slate-900 text-white p-2 rounded hover:bg-slate-700 transition  cursor-pointer">Sign In</button>
-
-
-            </form>
-            {/* login form ends */}
-
-
-            <p className="text-sm mt-2 lg:mt-4">Don't have an account?
-                <span className="text-blue-500 hover:underline cursor-pointer" onClick={() => navigate("/register")}> Sign Up</span>
-            </p>
-
-
-
-
-
-        </AuthLayout>
+                <p className="text-sm mt-2 lg:mt-4">Don't have an account?
+                    <span className="text-blue-500 hover:underline cursor-pointer" onClick={() => navigate("/register")}> Sign Up</span>
+                </p>
+            </AuthLayout>
+        </>
 
     )
 }
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import AuthLayout from "../components/AuthLayout";
 import InputField from "../components/InputField";
+import { Helmet } from "react-helmet-async";
 
 
 type FormValues = {
@@ -33,35 +34,40 @@ const Register = () => {
     }
 
     return (
-        <AuthLayout title="Beauty Chat" sideContent="Welcome back!" >
+        <>
+            <Helmet>
+                <title>Register | Beauty Chat</title>
+                <meta name="description" content="Create a new account" />
+            </Helmet>
 
-            {/* Register form begins */}
-            <form onSubmit={handleSubmit(onSubmit)} className="space-y-5 ">
-                {
-                    fields.map((field) => (
-                        <div key={field.name}>
-                            <InputField
-                                type={field.type}
-                                placeholder={field.placeholder}
-                                register={register}
-                                name={field.name}
-                                error={errors[field.name as keyof FormValues]?.message as string}
-                            />
-                        </div>
-                    ))
-                }
+            <AuthLayout title="Beauty Chat" sideContent="Welcome back!" >
 
-                <button className="w-full bg-slate-900 text-white p-2 rounded hover:bg-slate-700 transition  cursor-pointer">Sign Up</button>
+                {/* Register form begins */}
+                <form onSubmit={handleSubmit(onSubmit)} className="space-y-5 ">
+                    {
+                        fields.map((field) => (
+                            <div key={field.name}>
+                                <InputField
+                                    type={field.type}
+                                    placeholder={field.placeholder}
+                                    register={register}
+                                    name={field.name}
+                                    error={errors[field.name as keyof FormValues]?.message as string}
+                                />
+                            </div>
+                        ))
+                    }
 
+                    <button className="w-full bg-slate-900 text-white p-2 rounded hover:bg-slate-700 transition  cursor-pointer">Sign Up</button>
+                </form>
+                {/* Register form ends */}
 
-            </form>
-            {/* Register form ends */}
+                <p className="text-sm mt-2 lg:mt-4">Already have an account?
+                    <span className="text-blue-500 hover:underline cursor-pointer" onClick={() => navigate("/login")}> Sign In</span>
+                </p>
 
-            <p className="text-sm mt-2 lg:mt-4">Already have an account?
-                <span className="text-blue-500 hover:underline cursor-pointer" onClick={() => navigate("/login")}> Sign In</span>
-            </p>
-
-        </AuthLayout>
+            </AuthLayout>
+        </>
     )
 }
 
