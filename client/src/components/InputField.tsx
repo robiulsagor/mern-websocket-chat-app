3c9968e9c901e0d9be0feba94786058a914dc0f5
robiulsagor <robiul100.me@gmail.com>
2025-09-26 21:00:48 +0600

    feat: enhance InputField component with animated error message display

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -1,4 +1,5 @@
 import type { FieldValues, Path, UseFormRegister } from "react-hook-form";
+import { AnimatePresence, motion } from "motion/react";
 
 type InputFieldProps<T extends FieldValues> = {
     type: string;
@@ -17,7 +18,10 @@ const InputField = <T extends FieldValues>({ type, placeholder, register, name, 
                 placeholder={placeholder}
                 className="w-full bg-slate-100 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
-            {error && <p className="mt-1 ml-1 text-red-700 text-sm">{error}</p>}
+            <AnimatePresence>
+                {error && <motion.p initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -10 }} transition={{ duration: 0.2 }}
+                    className="mt-1 ml-1 text-red-700 text-sm">{error}</motion.p>}
+            </AnimatePresence>
         </div>
     )
 }
